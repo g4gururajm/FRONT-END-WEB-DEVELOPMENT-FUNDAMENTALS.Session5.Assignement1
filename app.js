dc6f825c708e38383e209d97b24d184cd0f89078
g4gururajm <gururaj.tech@gmail.com>
2017-05-31 18:39:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+		/* Variable to store name*/
+		var  name = "Smith";
+		/* Variable to store age*/
+		var  age  = 29;
+		/* Variable to store date of birth*/
+		var dateofbirth = "01/01/1970";
+		/* Variable to store place of birth*/
+		var placeofbirth = "USA";
+		/* print on console that variable which store name*/
+		console.log("Name of the Person: " + name);
+		/* print on console that variable which store age*/
+		console.log("Age of the Person: " + age);
+		/* print on console that variable which store date of birth*/
+		console.log("Date of Birth: " + dateofbirth);
+		/* print on console that variable which store place of birth*/
+		console.log("Place of Birth: " + placeofbirth);
